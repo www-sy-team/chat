3ded494f2b3c076512422433b5727577d907c6d3
HuLa Deploy <deploy@hula.io>
2026-04-29 20:07:12 +0800

    Add MybatisPlusConfig

---
+++ luohuo-cloud/luohuo-game/luohuo-game-server/src/main/java/com/luohuo/game/config/MybatisPlusConfig.java
@@ -0,0 +1,15 @@
+package com.luohuo.game.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        return interceptor;
+    }
+}
